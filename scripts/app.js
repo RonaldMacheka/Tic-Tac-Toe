4771278c0fdb3b9016f6f7c474fbfb7d99b30284
ronald-macheka <ronymacheka@outlook.com>
2023-09-10 05:40:25 +0200

    added scripts which hides & shows the configuration modal

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
+const playerConfigOverlayElement = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
+const cancelConfigBtn = document.getElementById("cancel-config-btn");
 
 editPlayer1Btn.addEventListener("click", openPlayerConfig);
 editPlayer2Btn.addEventListener("click", openPlayerConfig);
+
+cancelConfigBtn.addEventListener("click", closePlayerConfig);
+backdropElement.addEventListener("click", closePlayerConfig);
